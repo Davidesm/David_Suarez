9c19c2d5b0dd84fdf602878b0d6acfa58158cacf
Davidesm <84057787+Davidesm@users.noreply.github.com>
2025-03-02 16:13:50 +0000

    Cargue actividad 1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="David Suarez",
+    author_email=" ",
+    description="",
+    py_modules=["Actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+)
